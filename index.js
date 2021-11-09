c1454780c59c0a34d6bf7c345b884da8744937ff
Dhanush <dhanu2kamrthi@gmail.com>
2021-11-09 08:02:33 +0530

    my message

---
+++ index.js
@@ -0,0 +1,10 @@
+function validdate(){
+    var username =document.getElementById("class").value
+var regex = /^[E00]$/;
+if(regex.test(username)){
+    alert("valid useranme");
+}else{
+alert("invalid username");
+}
+}
+
